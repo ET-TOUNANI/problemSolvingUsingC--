73a00b82c559cf0f002fd1dd3e397c22ed5a2917
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-10 13:23:13 -0800

    fixed the problem with Negative Array

--- First Missing Positive/main.cpp
+++ First Missing Positive/main.cpp
@@ -12,32 +12,62 @@ public:
     // find number in the array
     bool FindNumbers(vector<int> &nums, int number)
     {
-        for (int i = 0; i < nums.size(); i++)
+        for (auto i = nums.begin(); i != nums.end(); ++i)
         {
-            if (nums[i] == number)
+            if (*i == number)
                 return true;
         }
         return false;
+    }
+    // find small number in the array
+    int FindSmallNumbers(vector<int> &nums)
+    {
+        int small = nums[0];
+        for (auto i = nums.begin(); i != nums.end(); ++i)
+        {
+            if (*i < small)
+                small = *i;
+        }
+        return small;
     }
 
     int firstMissingPositive(vector<int> &nums)
     {
         // if the values of the Array Negative or not
 
-        for (int i = 0; i < nums.size(); i++)
+        for (auto i = nums.begin(); i != nums.end(); ++i)
         {
-            if (nums[i] >= 0)
+            if (*i >= 0)
                 continue;
             else
                 this->arrayIsNegative = true;
         }
+
         // there is a Negative value in the Array
         if (this->arrayIsNegative == true)
         {
+            vector<int> posArray;
+
+            for (auto i = nums.begin(); i != nums.end(); ++i)
+            {
+                if (*i >= 0)
+                {
+                    posArray.push_back(*i);
+                }
+                else
+                    continue;
+            }
+            for (int i = FindSmallNumbers(posArray) + 1;; i++)
+            {
+                if (FindNumbers(posArray, i) == true)
+                    continue;
+                else
+                    return i;
+            }
         }
         else
         {
-            int j = 0, res = 0;
+            int j = 1, res = 1;
 
         next:
             if (FindNumbers(nums, j++) == true)
@@ -55,11 +85,11 @@ public:
 int main()
 {
     vector<int> nums;
-    nums.push_back(0);
     nums.push_back(1);
-    nums.push_back(2);
-    nums.push_back(3);
-    nums.push_back(4);
+    nums.push_back(8);
+    nums.push_back(9);
+    nums.push_back(11);
+    nums.push_back(12);
     Solution s1;
     cout << s1.firstMissingPositive(nums) << endl;
     return 0;
