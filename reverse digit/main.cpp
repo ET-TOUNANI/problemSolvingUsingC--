945199909e8551019e69baf9d9c471039f6f3a54
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-18 15:21:06 -0800

    write ReverseDigit function
    this function transfer integer to array
    example
    12345 -> [5 - 4 - 3 - 2 - 1]

--- reverse digit/main.cpp
+++ reverse digit/main.cpp
@@ -3,6 +3,23 @@
 #include <vector>
 using namespace std;
 
+vector<int> reverseDigit(int x)
+{
+    vector<int> result;
+    while (x >= 1)
+    {
+        result.push_back(x % 10);
+        x = x / 10;
+    }
+    return result;
+}
+
 int main()
 {
+    int x = 123;
+    vector<int> res = reverseDigit(x);
+
+    for (int i = 0; i < ; i++)
+    {
+    }
 }
