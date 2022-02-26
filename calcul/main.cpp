4c079d33b4e375a26b81b67906a51916ea31cc74
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-26 22:14:07 +0100

    feat (main) : add Class Calc with function

--- calcul/main.cpp
+++ calcul/main.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 class Calc
 {
-
+    public :
+    double a,b;
+    double Add(){
+        return a+b;
+    }
+    double Sub(){
+        return a-b;
+    }
+    double Mult(){
+        return a*b;
+    }
+    double Div(){
+        double c;
+         if (b != 0) 
+            c = (double) a / b;
+        else return -1;
+        return c;
+    }
 };
 
 int main()
